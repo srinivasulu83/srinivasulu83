1c6b929f112c780fed88dc0fd1430baf1ecab5ed
srinivasulu83 <155417370+srinivasulu83@users.noreply.github.com>
2024-03-13 12:19:46 +0530

    Add files via upload

---
+++ Appl_Servlet_HttpSession/src/main/java/test/LogOutServlet.java
@@ -0,0 +1,24 @@
+package test;
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+@SuppressWarnings("serial")
+@WebServlet("/logout")
+public class LogOutServlet extends HttpServlet{
+	protected void doGet(HttpServletRequest req,HttpServletResponse res)
+	      throws ServletException,IOException{
+		PrintWriter pw=res.getWriter();
+		res.setContentType("text/html");
+		HttpSession hs=req.getSession(false);
+		if(hs==null) {
+			pw.println("Session expired..<>br");
+		}else {
+			hs.removeAttribute("ab");
+			hs.invalidate();//session destroyed
+			pw.println("logged out Successfully..<br>");
+		}
+		RequestDispatcher rd=req.getRequestDispatcher("home.html");
+		rd.include(req, res);
+	}
+}
